799a83c780ac2aabdf8701deebbb81f2203e7500
Kamil <xzwiadowca@gmail.com>
2019-11-15 11:36:56 +0100

    Add car, add dependenties

---
+++ src/main/java/pl/mpas/git_examples/car/model/Car.java
@@ -0,0 +1,14 @@
+package pl.mpas.git_examples.car.model;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class Car {
+    private String brand;
+    private String model;
+    private String color;
+    private BigDecimal price;
+    private int engine;
+
+}
